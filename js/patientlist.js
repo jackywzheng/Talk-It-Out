ecfa87f34364d742d294ebe21db1f659774f4c6a
Yongju Kwon <yongjukwon@Yongjus-MacBook-Pro.local>
2019-07-14 11:57:36 -0700

    redireted all paths

--- js/patientlist.js
+++ js/patientlist.js
@@ -1,4 +1,4 @@
-let comprehendmedical = new AWS.ComprehendMedical({apiVersion: '2018-10-30', region:'us-west-2', accessKeyId:'AKIASJF5PNYJ4DR3OETF', secretAccessKey:'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'});
+/* let comprehendmedical = new AWS.ComprehendMedical({apiVersion: '2018-10-30', region:'us-west-2', accessKeyId:'AKIASJF5PNYJ4DR3OETF', secretAccessKey:'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'});
 let id = 100;
 
 function concatenateInput() {
@@ -26,7 +26,6 @@ function submitData(params) {
         }
     });
     results.unshift(id);
-    id++;
     return results;
 }
 
@@ -35,5 +34,39 @@ function submitForm() {
         Text: concatenateInput()
     };
     let results = submitData(param);
-    return results
+    setData(results);
+    id++;
 }
+
+function setData(data) {
+    let container = document.createElement("div");
+    container.class = "container";
+    container.id = "container" + id;
+    let card = document.createElement("div");
+    card.class = "card";    
+    let cardHeader = document.createElement("div")
+    cardHeader.class = "card-header patientId"; 
+    cardHeader.id = "patient" + id;
+    let cardBody = document.createElement("div");
+    cardBody.class = "card-body patientWord";
+    let cardText = document.createElement("p")
+    cardText.class = "card-text";
+    cardText.id = "data" + id;
+
+    document.getElementById('patientbody').appendChild(container);
+    container.appendChild(card);
+    card.appendChild(cardHeader);
+    card.appendChild(cardBody);
+    cardBody.appendChild(cardText);
+
+    cardHeader.innerHTML = data[0];
+    if (data.length >= 2){
+        let ul = document.createElement("ul");
+        for (let i = 1; i < data.length; i++){
+            let li = document.createElement("li");
+            li.innerHTML = data[i];
+            ul.appendChild(li);
+        }
+    }
+}
+ */
